4c195f89c9fc18bbc6b62b2bdc2afd3ca4d4e487
Richard_Oliver_Legen <richard_oliver_legendi@epam.com>
2023-09-23 12:54:20 +0200

    Adding DB inint part

--- flaskr/db.py
+++ flaskr/db.py
@@ -20,3 +20,18 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    # Not to self - context manager here, init/enter/exit functions
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
