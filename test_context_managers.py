340549cfc87fce4d7fd0446b400323fc5d8bfb4f
Richard_Oliver_Legen <richard_oliver_legendi@epam.com>
2023-09-23 12:58:07 +0200

    Testing context managers

---
+++ test_context_managers.py
@@ -0,0 +1,14 @@
+class MyContextManager:
+    def __init__(self, msg):
+        self.msg = msg
+        print("init", msg)
+
+    def __enter__(self):
+        print("enter")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("exit")
+
+
+with MyContextManager("mcm") as cm:
+    print(">> actual work")
