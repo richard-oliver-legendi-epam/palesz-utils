f6e59998370935adaf30ce16ebb5806a467588e9
Richard_Oliver_Legen <richard_oliver_legendi@epam.com>
2023-09-24 23:30:15 +0200

    Detailing test

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,3 +1,5 @@
 def test_index(client):
     response = client.get('/')
     assert b'Kalkul\xc3\xa1torok' in response.data
+    assert (b'Ezeket meguntam k\xc3\xa9zzel sz\xc3\xa1molgatni, ink\xc3\xa1bb csin\xc3\xa1ltam egy appot :-)'
+            in response.data)
