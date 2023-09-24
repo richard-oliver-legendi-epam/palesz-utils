54fa7aa90bc865da957bc759edec5bcd51094ced
Richard_Oliver_Legen <richard_oliver_legendi@epam.com>
2023-09-24 23:27:52 +0200

    Separation tests complete

--- tests/test_separation.py
+++ tests/test_separation.py
@@ -1,3 +1,19 @@
-def test_separation(client):
+def test_separation_get(client):
     response = client.get('/separation')
+
+    assert b'Alszes mennyis\xc3\xa9ge f\xc5\x91z\xc3\xa9s el\xc5\x91tt (liter)' in response.data
     assert b'Az el\xc5\x91p\xc3\xa1rlatra vonatkoz\xc3\xb3 mennyis\xc3\xa9gek:' in response.data
+
+
+def test_separation_post(client):
+    response = client.post('/separation', data={'volume': 80})
+
+    assert b'4.00 dL - 16.00 dL' in response.data
+    assert b'2.40 dL - 4.00 dL' in response.data
+    assert b'4.00 dL - 12.00 dL' in response.data
+
+
+def test_separation_errors(client):
+    response = client.post('/separation', data={'volume': 0})
+
+    assert b'A liter pozit\xc3\xadv kell legyen!' in response.data
