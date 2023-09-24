9f843b45480fd4484c958091b326512daad3729a
Richard_Oliver_Legen <richard_oliver_legendi@epam.com>
2023-09-24 16:55:13 +0200

    Dilution working

--- flaskr/util.py
+++ flaskr/util.py
@@ -12,16 +12,22 @@ bp = Blueprint('util', __name__)
 
 # Use single-quotes for string literals, e.g. 'my-identifier' , but use double-quotes for strings that are likely to contain single-quote characters as part of the string itself
 
+# TODO error handling, like target % is higher than act %, etc.
+
 
 @bp.route('/dilution', methods=('GET', 'POST'))
 def dilution():
     if request.method == 'POST':
         try:
             volume: float = float(request.form['volume'])
-            act_alcohol_level : float = float(request.form['act_alcohol_level'])
-            target_alcohol_level : float= float(request.form['target_alcohol_level'])
+            act_alcohol_level: float = float(request.form['act_alcohol_level'])
+            target_alcohol_level: float = float(request.form['target_alcohol_level'])
 
-            return render_template('util/dilution.html')
+            output_value: float = (volume * act_alcohol_level / target_alcohol_level) - volume
+            output: str = (f"{volume:.2f} * {act_alcohol_level:.2f} / {target_alcohol_level:.2f} - {volume:.2f} "
+                           f"== {output_value:.2f} deciliter vízre van szükség")
+
+            return render_template('util/dilution.html', output=output)
         except Exception as e:
             flash(str(e))
             return render_template('util/dilution.html')
